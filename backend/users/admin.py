d2b8da8a38693d695eef3cfd331e345f34cc9337
vladvm <vobrembalskii@gmail.com>
2022-10-30 14:02:25 +0300

    github workflow

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,13 +3,13 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# Register your models here.
+
 class CastomUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name')
-    #list_editable = ('color',)
-    search_fields = ('username','email')
-    list_filter = ('username','email')
+    search_fields = ('username', 'email')
+    list_filter = ('username', 'email')
     empty_value_display = '-пусто-'
+
 
 admin.site.unregister(User,)
 admin.site.register(User, CastomUserAdmin)
